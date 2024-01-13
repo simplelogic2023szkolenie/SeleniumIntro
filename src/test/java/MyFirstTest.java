973bf6021b4e99e8174eb9c1b3ec7a952710fe55
Mateusz Tadla <mat@scalepoint.com>
2024-01-13 15:54:09 +0100

    update

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,9 +1,32 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {
+    @AfterMethod
+    public void cleanup(){
+        System.out.println(">>>Po kazdej metodzie testowej!");
+    }
+    @BeforeMethod
+    public void setup(){
+        System.out.println("<<<Przed każdą metodą testową!");
+    }
 
     @Test
-    public void someTest(){
-        System.out.println("this is my first test");
+    public void shouldAddTwoZeros(){
+        System.out.println("start testu 0+0");
+        int result = add(0,0);
+        Assert.assertEquals(result,0);
+        System.out.println("koniec testu 0+0");
+    }
+    public int add(int a, int b){
+        return a+a;
+    }
+    @Test
+    public void shouldAddTwoPositiveNumbers(){
+        System.out.println("start testu 1+3");
+        Assert.assertEquals(add(1,3),4);
+        System.out.println("koniec testu 1+3");
     }
 }
